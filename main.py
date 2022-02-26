ab8970ebefb9d612a537b7441737691842a0cb44
206 <206@academicos.cesde.edu.co>
2022-02-25 20:53:02 -0500

    nuevo commit

--- main.py
+++ main.py
@@ -2,3 +2,10 @@ nivelAgua = int(input("Digita la cantidad de agua de la represa: "))
 print(f"la cantidad de agua es {nivelAgua}")
 
 print("hola mundo")
+
+if(nivelAgua < 200):
+    print('no tengo agua')
+elif(nivelAgua >= 200 and nivelAgua < 450):
+    print('todo bien... energia funcionando')
+else:
+    print('ojo con caucasia')
